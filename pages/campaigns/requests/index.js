b3786bcfd9ec82c816945ac3f440cea65b6ec288
assohail <aamirsohail9210@gmail.com>
2022-10-15 00:01:16 +0500

    added new routes, resolved issues

--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -1,20 +1,64 @@
 import Link from "next/link";
 import React, { Component } from "react";
-import { Button } from "semantic-ui-react";
+import { Button, Tab, Table } from "semantic-ui-react";
 import Layout from "../../../components/Layout";
-
+import Campaign from "../../../ethereum/campaign";
+import RequestRow from "../../../components/requestRow";
 class RequestIndex extends Component {
     static async getInitialProps(props) {
         const address = props.query.address;
-        return { address };
+        //@ Campaign(address) ?
+        const campaign = Campaign(address);
+        const requestCount = await campaign.methods.getRequestsCount().call();
+        const approversCount = await campaign.methods.approversCount().call();
+
+        const requests = await Promise.all(
+            Array(parseInt(requestCount)).fill().map((element, index) => {
+                return campaign.methods.requests(index).call()
+            })
+        );
+        return { address, requests, requestCount, approversCount };
+    }
+
+    renderRows(){
+        return this.props.requests.map((request, index) => {
+            return (
+                <RequestRow
+                //key is required from react when rendering list of components 
+                key={index}
+                id={index}
+                request={request}
+                address={this.props.address}
+                approversCount={this.props.approversCount}
+                />
+            )
+    })
     }
     render () {
+        const {Header, Row, HeaderCell, Body} = Table;
         return(
          <Layout>
             <h3>RI</h3>
                <Link href={`/campaigns/${this.props.address}/requests/new`}>
-                    <Button content="Add Request" primary/>
+                    <Button primary floated="right" style={{marginBottom: 10}}>Add Request</Button>
                 </Link> 
+                <Table>
+                    <Header>
+                        <Row>
+                            <HeaderCell>ID</HeaderCell>
+                            <HeaderCell>Description</HeaderCell>
+                            <HeaderCell>Amount</HeaderCell>
+                            <HeaderCell>Recepient</HeaderCell>
+                            <HeaderCell>Approval</HeaderCell>
+                            <HeaderCell>Approve</HeaderCell>
+                            <HeaderCell>Finalize</HeaderCell>
+                        </Row>
+                    </Header>
+                    <Body>
+                        {this.renderRows()}
+                    </Body>
+                </Table>
+                <div>Found {this.props.requestCount} requests</div>
          </Layout>
         )
     }
--- pages/campaigns/requests/new.js
+++ pages/campaigns/requests/new.js
@@ -1,24 +1,51 @@
 import React, {Component} from "react";
-import { Button, Form, Input } from "semantic-ui-react";
+import { Button, Form, Input, Message } from "semantic-ui-react";
 import Campaign from '../../../ethereum/campaign'
 import web3 from '../../../ethereum/web3'
 import Layout from "../../../components/Layout";
+import Link from "next/link";
+import { Router } from "next/router";
 
 class RequestNew extends Component {
     state = {
         value: '',
         description: '',
-        recepient: ''
+        recepient: '',
+        loading: false,
+        errorMessage: ''
     };
-    static async getInitialPtops(props){
-         const address = props.query;
+    static async getInitialProps(props){
+         const { address } = props.query;
          return { address };
+    }
+    onSubmit = async (event) => {
+        event.preventDefault();
+
+        const campaign = await Campaign(this.props.address)
+        const {description, value, recepient } = this.state;
+        const accounts = await web3.eth.getAccounts();
+        this.setState({loading: true, errorMessage:''})
+        try {
+            await campaign.methods.createRequest(
+                description, web3.utils.toWei(value, 'ether'), recepient)
+                .send({
+                    from: accounts[0],
+                    gas: '1000000'
+                })
+          Router.push(`/campaigns/${this.props.address}/requests`)
+        } catch (error) {
+            this.setState({errorMessage: error.message});
+        }
+        this.setState({loading: false});
     }
     render() {
         return (
           <Layout>
+            <Link href={`/campaigns/${this.props.address}/requests`}>
+                <a>Back</a>
+            </Link>
             <h3>Create Request</h3>
-            <Form>
+            <Form onSubmit={this.onSubmit} error={!!this.state.errorMessage}>
                 <Form.Field>
                     <label>Description</label>
                     <Input 
@@ -40,7 +67,8 @@ class RequestNew extends Component {
                         onChange={event => this.setState({recepient: event.target.value})}
                     />
                 </Form.Field>
-                <Button content="Create!" primary />
+                <Message error header="Oops" content={this.state.errorMessage} />
+                <Button type="submit" primary loading={this.state.loading}>Create!</Button>
             </Form>
           </Layout>
         )   
