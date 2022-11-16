32dc750d7403529b705f3e4f638c633b2c2997ad
assohail <aamirsohail9210@gmail.com>
2022-11-16 23:29:26 +0500

    .env, testnet config

--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,5 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+
 module.exports = {
   networks: {
     development: {
@@ -6,16 +8,24 @@ module.exports = {
       network_id: "*", // Match any network id
       gas: 5000000
     },
-  },
-  compilers: {
-    solc: {
-      settings: {
-        optimizer: {
+    rinkeby: {           
+      provider: () => new HDWalletProvider(process.env.MNEMONIC, "https://rinkeby.infura.io/v3/"+process.env.INFURA_API_KEY),
+      network_id: 4,    
+    },
+    sepolia: {           
+      provider: () => new HDWalletProvider(process.env.MNEMONIC, "https://sepolia.infura.io/v3/c24f61d37f764ef0841ed632141835d0"),
+      network_id: 11155111,    
+    },
+    compilers: {
+      solc: {
+        settings: {
+          optimizer: {
           enabled: true, // Default: false
           runs: 200,      // Default: 200
-        }
-      },
+          }
+        },
       version: "^0.8.0"
     }
-  }
-};
+  },
+ },
+}
